7487650ccf15d1ab84c1d3de07a22ac8690e7a0c
Caram Dache <caramdache@gmail.com>
2022-06-06 22:28:18 +0200

    Move forms to a separate file.

---
+++ dynfilters/forms.py
@@ -0,0 +1,54 @@
+from operator import itemgetter
+
+from django import forms
+
+from adminsortable2.admin import CustomInlineFormSet
+
+from .models import DynamicFilterTerm
+
+
+class DynamicFilterTermInlineFormSet(CustomInlineFormSet):
+    pass
+
+
+class DynamicFilterTermInlineForm(forms.ModelForm):
+    class Meta:
+        model = DynamicFilterTerm
+        fields = ('op', 'field', 'lookup', 'value', 'bilateral', 'order')
+
+    def clean(self):
+        errors = {}
+
+        op, field, lookup, value = itemgetter('op', 'field', 'lookup', 'value')(self.cleaned_data)
+
+        if op in ('-', '!'):
+            if field == '-':
+                errors.update({'field': 'Missing value'})
+
+            if lookup == '-':
+                errors.update({'lookup': 'Missing value'})
+
+            if not value:
+                if lookup in ('-', 'iexact', 'icontains', 'range', 'lt', 'gt', 'lte', 'gte'):
+                    errors.update({'value': 'Missing value'})
+
+            else:
+                if 'date' in field:
+                    if lookup == 'range':
+                        try:
+                            str_as_date_range(value)
+                        except:
+                            errors.update({'value': 'Should be "DD/MM/YYYY, DD/MM/YYYY"'})
+
+                    else:
+                        try:
+                            str_as_date(value)
+                        except:
+                            errors.update({'value': 'Should be "DD/MM/YYYY"'})
+
+
+                if lookup in ('lt', 'gt', 'lte', 'gte'):
+                    pass
+
+        if errors:
+            raise ValidationError(errors)
